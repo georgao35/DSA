cafd24c033dbbbe988794be555e4de887c182a77
gjy <gaojy19@mails.tsinghua.edu.cn>
2020-09-17 19:10:47 +0800

    vectors append

--- templates/vectors.cpp
+++ templates/vectors.cpp
@@ -2,22 +2,43 @@
 using namespace std;
 
 template <class T>
-class vector{
+class Vector{
     int _size;
     T* _content;
     int _length;
 public:
+    Vector<T>();
+    Vector<T>(const Vector<T>& src);
+    Vector<T>(Vector<T>&& src);
+
     void expand();
-    void append();
+    void append(const T&);
     void deduplicate();
     void uniquify();
+
+    T& operator[](int rank);
 
     T& binSearch(const T& e, int lo, int hi);
 };
 
 template <class T>
-void vector<T>::expand(){
-    if(_size == _length){
-        
-    }
+void Vector<T>::expand(){
+    if(_size < _length)
+        return;
+    T* old_content = _content;
+    _content = new T [2*_length];
+    for(int i=0;i<_length;++i) _content[i] = old_content[i];
+    delete [] old_content;
+    _length *= 2;
+}
+
+template <class T>
+void Vector<T>::append(const T& src){
+    expand();
+    _content[_size++] = src;
+}
+
+template <class T>
+T& Vector<T>::operator[](int rank){
+    if(rank >= 0 && rank <_size) return _content[rank];
 }
