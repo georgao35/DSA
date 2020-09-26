1636202df23b826e5844f7746d6667fd62f8ea6c
georgao35 <gjy@LAPTOP-BV4FO568.localdomain>
2020-09-26 20:51:26 +0800

    9/26

--- PA1/4.cpp
+++ PA1/4.cpp
@@ -3,8 +3,8 @@
 #include <cstring>
 using namespace std;
 
-char src[3200005];//初始的字符串
-int n, leftId, rightId;
+char src[105];//初始的字符串
+int leftId, rightId, len;
 
 struct Node{
     Node* pred; 
@@ -16,19 +16,72 @@ struct Node{
     Node* insertAsPred(char e){
         Node* tmp = new Node(e, pred, this);
         pred->succ = tmp; pred = tmp;
+        len++;
         return tmp;
     }
     Node* insertAsSucc(char e){
         Node* tmp = new Node(e, this, succ);
         succ->pred = tmp; succ = tmp;
+        len++;
         return tmp;
     }
 };
 Node* Left, *Right;
 Node* header, *trailer;
 
-void moveRight(){}
-void moveLeft(){}
+void moveRight(){
+    char which;//判断左右
+    scanf("%c", &which);
+    switch (which)
+    {
+    case 'L':
+        if(leftId == len)
+            printf("F\n");
+        else{
+            Left = Left->succ;
+            leftId++;
+            printf("T\n");
+        }
+        break;
+    case 'R':
+        if(rightId == len)
+            printf("F\n");
+        else{
+            Right = Right->succ;
+            rightId++;
+            printf("T\n");
+        }
+        break;
+    default:
+        break;
+    }
+}
+void moveLeft(){
+    char which;
+    scanf("%c", &which);
+    switch (which)
+    {
+    case 'L':
+        if(leftId == 0)
+            printf("F\n");
+        else{
+            Left = Left->pred;
+            leftId--;
+            printf("T\n");
+        }
+        break;
+    case 'R':
+        if(rightId == 0)
+            printf("F\n");
+        else{
+            Right = Right->pred;
+            rightId--;
+            printf("T\n");
+        }
+    default:
+        break;
+    }
+}
 void insert(){}
 void del(){}
 void reverse(){}
@@ -37,8 +90,8 @@ void init(){
     header = new Node; trailer = new Node;
     header->succ = trailer; header->pred = nullptr;
     trailer->pred = header; trailer->succ = nullptr;
-    int len = strlen(src);
-    for(int i=0; i<len; ++i){
+    int l = strlen(src);
+    for(int i=0; i<l; ++i){
         trailer->insertAsPred(src[i]);
     }
     Left = header->succ; leftId = 0;
@@ -49,6 +102,7 @@ int main(){
     scanf("%s",src);
     leftId = 0; rightId = strlen(src);
     init();
+    int n;
     scanf("%d",&n);
     while(n-- > 0){
         char manip;
